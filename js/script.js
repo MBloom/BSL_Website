53f585a7f1c11692728643896c8428f196e91dbb
Matt Bloom <msb3ba@virginia.edu>
2014-06-05 16:30:13 -0400

    contact page

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // collapse the navbar on scroll
 $(window).scroll(function() {
-    if ($('body').is('.HomePage')){
+    if ($('body').is('.HomePage')) {
         if ($(".navbar").offset().top > 520) {
             $(".navbar-fixed-top").addClass("top-nav-collapse");
         } else {
@@ -8,166 +8,145 @@ $(window).scroll(function() {
         }
     }
 });
-
 /* 
 Document vs. Window
 $( document ) is only available after the document has loaded
 $( window ) is available before the document loads/is ready
 */
-
-$(document).ready( function(){
-    $( "#grads" ).click(function() {
+$(document).ready(function() {
+    $("#grads").click(function() {
         // simply switch content if other section is currrently active 
         if ($("#pros-section").is(':visible')) {
-            $( ".pros-toggle" ).toggle();
-            $( ".grads-toggle" ).toggle();
+            $(".pros-toggle").toggle();
+            $(".grads-toggle").toggle();
         }
         // toggle section
-        else
-            $( ".grads-toggle" ).toggle( "blind" );
+        else $(".grads-toggle").toggle("blind");
         //move to top of the section
         if ($("#grads-section").is(':visible')) {
-            $("html, body").animate({scrollTop: $("#grads-section").offset().top});
+            $("html, body").animate({
+                scrollTop: $("#grads-section").offset().top
+            });
         }
     });
-
     // professional image is clicked
-    $( "#pros" ).click(function() {
+    $("#pros").click(function() {
         // simply switch content if other section is currrently active 
         if ($("#grads-section").is(':visible')) {
-            $( ".grads-toggle" ).toggle();
-            $( ".pros-toggle" ).toggle();
+            $(".grads-toggle").toggle();
+            $(".pros-toggle").toggle();
         }
         // toggle section
-        else
-            $( ".pros-toggle" ).toggle( "blind" );
+        else $(".pros-toggle").toggle("blind");
         //move to top of the section
         if ($("#pros-section").is(':visible')) {
-            $("html, body").animate({scrollTop: $("#pros-section").offset().top});
+            $("html, body").animate({
+                scrollTop: $("#pros-section").offset().top
+            });
         }
     });
 });
-(function($){
+(function($) {
     // Parallax
     $('.bkg').each(function() {
         $(this).parallax('50%', 0.5, true);
     });
 })(jQuery);
-
-//Google Map Skin - Get more at http://snazzymaps.com/
-// var myOptions = {
-//     zoom: 15,
-//     center: new google.maps.LatLng(53.385873, -1.471471),
-//     mapTypeId: google.maps.MapTypeId.ROADMAP,
-//     disableDefaultUI: true,
-//     styles: [{
-//         "featureType": "water",
-//         "elementType": "geometry",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 17
-//         }]
-//     }, {
-//         "featureType": "landscape",
-//         "elementType": "geometry",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 20
-//         }]
-//     }, {
-//         "featureType": "road.highway",
-//         "elementType": "geometry.fill",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 17
-//         }]
-//     }, {
-//         "featureType": "road.highway",
-//         "elementType": "geometry.stroke",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 29
-//         }, {
-//             "weight": 0.2
-//         }]
-//     }, {
-//         "featureType": "road.arterial",
-//         "elementType": "geometry",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 18
-//         }]
-//     }, {
-//         "featureType": "road.local",
-//         "elementType": "geometry",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 16
-//         }]
-//     }, {
-//         "featureType": "poi",
-//         "elementType": "geometry",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 21
-//         }]
-//     }, {
-//         "elementType": "labels.text.stroke",
-//         "stylers": [{
-//             "visibility": "on"
-//         }, {
-//             "color": "#000000"
-//         }, {
-//             "lightness": 16
-//         }]
-//     }, {
-//         "elementType": "labels.text.fill",
-//         "stylers": [{
-//             "saturation": 36
-//         }, {
-//             "color": "#000000"
-//         }, {
-//             "lightness": 40
-//         }]
-//     }, {
-//         "elementType": "labels.icon",
-//         "stylers": [{
-//             "visibility": "off"
-//         }]
-//     }, {
-//         "featureType": "transit",
-//         "elementType": "geometry",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 19
-//         }]
-//     }, {
-//         "featureType": "administrative",
-//         "elementType": "geometry.fill",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 20
-//         }]
-//     }, {
-//         "featureType": "administrative",
-//         "elementType": "geometry.stroke",
-//         "stylers": [{
-//             "color": "#000000"
-//         }, {
-//             "lightness": 17
-//         }, {
-//             "weight": 1.2
-//         }]
-//     }]
-// };
-
-// var map = new google.maps.Map(document.getElementById('map'), myOptions);
+// Google Map Skin - Get more at http://snazzymaps.com/
+function init() {
+    var myLoc = new google.maps.LatLng(38.9394730, -77.1787780)
+    var myOptions = {
+        zoom: 12,
+        center: new google.maps.LatLng(38.9185625, -77.0995465),
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        scrollwheel: false,
+        streetViewControl: false,
+        styles: [{
+            "featureType": "landscape",
+            "stylers": [{
+                "visibility": "simplified"
+            }, {
+                "color": "#EEEEEE"
+            }]
+        }, {
+            "featureType": "road",
+            "elementType": "labels",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "road.highway",
+            "elementType": "geometry",
+            "stylers": [{
+                "visibility": "simplified"
+            }, {
+                "color": "#73acd9"
+            }]
+        }, {
+            "featureType": "road.arterial",
+            "elementType": "geometry",
+            "stylers": [{
+                "visibility": "simplified"
+            }, {
+                "color": "#FFFFFF"
+            }]
+        }, {
+            "featureType": "road.local",
+            "elementType": "geometry",
+            "stylers": [{
+                "visibility": "simplified"
+            }, {
+                "color": "#cccccc"
+            }]
+        }, {
+            "featureType": "administrative.province",
+            "elementType": "labels.text",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "administrative",
+            "elementType": "labels.text",
+            "stylers": [{
+                "visibility": "on"
+            }, {
+                "color": "#074575"
+            }]
+        }, {
+            "featureType": "administrative",
+            "elementType": "labels.text.stroke",
+            "stylers": [{
+                "visibility": "off"
+            }]
+        }, {
+            "featureType": "water",
+            "stylers": [{
+                "visibility": "simplified"
+            }, {
+                "color": "#AAAAAA"
+            }]
+        }, {
+            "featureType": "poi",
+            "stylers": [{
+                "visibility": "off"
+            }, {
+                "color": "#b9d6ec"
+            }]
+        }]
+    };
+    var map = new google.maps.Map(document.getElementById('map'), myOptions);
+    var marker = new google.maps.Marker({
+        position: myLoc,
+        animation: google.maps.Animation.DROP,
+        clickable: true,
+        map: map,
+        title: 'BluestoneLogic'
+    });
+    var infowindow = new google.maps.InfoWindow({
+        content: '<strong>BluestoneLogic</strong><br/>1313 Dolley Madison Blvd<br>McLean,VA 22101'
+    })
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.open(map, marker);
+    });
+}
+google.maps.event.addDomListener(window, 'load', init);
